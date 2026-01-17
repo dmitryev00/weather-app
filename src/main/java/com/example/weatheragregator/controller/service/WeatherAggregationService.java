dfa70b519291284854f691d4c9aeec3301ec9bc9
dmitryev00 <dmitryev00@inbox.ru>
2026-01-17 17:53:04 +0300

    logs have been added to the aggregation service to check for up-to-date information from the API

--- src/main/java/com/example/weatheragregator/controller/service/WeatherAggregationService.java
+++ src/main/java/com/example/weatheragregator/controller/service/WeatherAggregationService.java
@@ -3,22 +3,42 @@ package com.example.weatheragregator.controller.service;
 import com.example.weatheragregator.controller.service.provider.WeatherProvider;
 import com.example.weatheragregator.dto.provider.internal.WeatherData;
 import com.example.weatheragregator.dto.provider.internal.WeatherResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
 public class WeatherAggregationService {
 	private final List<WeatherProvider> providers;
+	private static final Logger log = LoggerFactory.getLogger(WeatherAggregationService.class);
 
 	public WeatherAggregationService(List<WeatherProvider> providers) {
 		this.providers = providers;
+		log.info("Инициализирован WeatherAggregationService с {} провайдерами", providers.size());
+
 	}
 
 	public WeatherResponse getWeather(String city) {
+
+		log.debug("Начало агрегации для города: {}", city);
+
 		List<WeatherData> allData = providers.stream()
-				.map(provider -> provider.getWeather(city))
+				.map(provider -> {try {
+					WeatherData data = provider.getWeather(city);
+					log.debug("Данные от {}: {}",
+							provider.getClass().getSimpleName(), data);
+					return data;
+				} catch (Exception e) {
+					log.error("Ошибка в провайдере {}: {}",
+							provider.getClass().getSimpleName(), e.getMessage());
+					return null;
+				}
+				})
+				.filter(Objects::nonNull)
 				.collect(Collectors.toList());
 
 		double avgTemp = allData.stream()
